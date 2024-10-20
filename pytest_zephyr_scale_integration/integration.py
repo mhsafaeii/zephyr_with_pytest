a3cf3aa7b9519212848a811671e0343a05846075
mhsafaeii <126967121+mhsafaeii@users.noreply.github.com>
2024-10-20 15:54:22 +0330

    Update integration.py

--- pytest_zephyr_scale_integration/integration.py
+++ pytest_zephyr_scale_integration/integration.py
@@ -1,6 +1,6 @@
 import os
 import time
-
+from datetime import datetime
 from dotenv import load_dotenv
 import requests
 from requests import HTTPError
@@ -15,59 +15,54 @@ JIRA_URL = None
 class Integration:
     def __init__(self, jira_token):
         self.session = requests.Session()
-        self.max_retries = 5  # Максимальное количество повторных попыток
-        self.retry_delay = 1  # Начальная задержка перед повторной попыткой (в секундах)
+        self.max_retries = 5
+        self.retry_delay = 1
 
         self.JIRA_TOKEN = jira_token
         self.JIRA_PROJECT_ID = None
         self.JIRA_URL = None
         self.folder_name = None
 
-        # Установка заголовков для сессии
+
         self.session.headers.update({
             'Authorization': f'Bearer {self.JIRA_TOKEN}',
             'Content-Type': 'application/json'
         })
 
     def load_environment_variables(self):
-        """Загрузка переменных окружения из .env файла"""
 
         load_dotenv()
-
-        # Получение значений из переменных окружения
+        date = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
         self.JIRA_PROJECT_ID = int(os.getenv("JIRA_PROJECT_ID"))
         self.JIRA_URL = os.getenv("JIRA_URL")
-        self.folder_name = os.getenv("FOLDER_NAME", None)
+        self.folder_name = f'Regression {date}'#os.getenv("FOLDER_NAME", None)
 
-        # Проверка на наличие обязательных переменных
         missing_env_vars = [var for var in ["JIRA_TOKEN", "JIRA_PROJECT_ID", "JIRA_URL"] if not getattr(self, var)]
         if missing_env_vars:
             raise EnvironmentError(f"Missing required environment variables: {', '.join(missing_env_vars)}")
         else:
             print(
-                f'Переменные загружены: {self.JIRA_TOKEN} \t {self.JIRA_PROJECT_ID} \t '
+                f'Variables loaded: {self.JIRA_TOKEN} \t {self.JIRA_PROJECT_ID} \t '
                 f'{self.JIRA_URL} \t {self.JIRA_PROJECT_ID}')
 
     def _send_request_with_retries(self, method, url, **kwargs):
-        """Отправка запроса с повторными попытками при статусе 429"""
 
         retries = 0
         while retries < self.max_retries:
             response = self.session.request(method, url, **kwargs)
             if response.status_code == 429:
                 retries += 1
-                wait_time = self.retry_delay * (2 ** (retries - 1))  # Экспоненциальная задержка
-                print(f"Превышен лимит количества отправленный сообщений. "
-                      f"Ожидаю {wait_time} секунд до повторной отправки...")
+                wait_time = self.retry_delay * (2 ** (retries - 1))
+                print(f"The limit of the number of messages sent has been exceeded. "
+                      f"Waiting {wait_time} seconds before resending...")
                 time.sleep(wait_time)
             else:
                 response.raise_for_status()
                 return response
-        raise HTTPError(f"Не удалось выполнить запрос после {self.max_retries} "
-                        f"попыток из-за ограничений скорости отправки запросов.")
+        raise HTTPError(f"Failed to complete request after {self.max_retries} "
+                        f" attempts due to rate limits on sending requests.")
 
     def get_project_key_by_project_id(self):
-        """Получение ключа проекта по его ID"""
 
         url = f"{self.JIRA_URL}/rest/tests/1.0/project/{self.JIRA_PROJECT_ID}"
         response = self.session.get(url)
@@ -79,7 +74,6 @@ class Integration:
         return response.json().get('key')
 
     def create_test_cycle(self, cycle_name, folder_id=None):
-        """Создание тестового цикла"""
         test_cycle_statuses = self.get_test_cycle_statuses()
         test_cycle_status_id = None
         for test_cycle_status in test_cycle_statuses:
@@ -105,7 +99,6 @@ class Integration:
         return response.json().get('id')
 
     def create_test_run_folder(self, folder_name):
-        """Создание новой папки для тестового цикла"""
 
         url = f"{self.JIRA_URL}/rest/tests/1.0/folder/testrun"
         payload = {
@@ -119,10 +112,9 @@ class Integration:
         print(data.decode('utf-8'))
 
         response.raise_for_status()
-        return response.json().get('id')  # Возвращаем ID новой папки
+        return response.json().get('id')
 
     def get_test_run_folders(self):
-        """Получение дерева папок тестовых циклов"""
 
         url = f"{self.JIRA_URL}/rest/tests/1.0/project/{self.JIRA_PROJECT_ID}/foldertree/testrun"
         response = self._send_request_with_retries('GET', url)
@@ -133,7 +125,6 @@ class Integration:
         return response.json()
 
     def get_test_case_id(self, project_key, test_case_key):
-        """Получение ID тест-кейса по ключу проекта и ключу тест-кейса"""
 
         url = f"{self.JIRA_URL}/rest/tests/1.0/testcase/{project_key}-{test_case_key}?fields=id"
         response = self._send_request_with_retries('GET', url)
@@ -145,7 +136,6 @@ class Integration:
         return response.json().get('id')
 
     def get_test_run_id(self, test_cycle_key):
-        """Получение ID тестового цикла"""
         url = f"{self.JIRA_URL}/rest/tests/1.0/testrun/{test_cycle_key}?fields=id"
         response = self._send_request_with_retries('GET', url)
 
@@ -156,7 +146,6 @@ class Integration:
         return response.json().get('id')
 
     def add_test_cases_to_cycle(self, test_run_id, test_case_ids):
-        """Добавление тест-кейсов в тестовый цикл"""
 
         url = f"{self.JIRA_URL}/rest/tests/1.0/testrunitem/bulk/save"
         added_test_run_items = [
@@ -175,7 +164,6 @@ class Integration:
         response.raise_for_status()
 
     def get_test_run_items(self, test_run_id):
-        """Получение всех тестов из тестового цикла"""
 
         url = (f"{self.JIRA_URL}/rest/tests/1.0/testrun/{test_run_id}/testrunitems?"
                f"fields=testCaseId,testScriptResults(id),testRunId")
@@ -188,7 +176,6 @@ class Integration:
         return response.json().get('testRunItems', [])
 
     def get_test_script_results(self, test_run_id, item_id):
-        """Получение результатов тестовых скриптов (параметризованных ТК)"""
 
         url = (f"{self.JIRA_URL}/rest/tests/1.0/testrun/{test_run_id}"
                f"/testresults?fields=testScriptResults(id,parameterSetId)&itemId={item_id}")
@@ -201,7 +188,6 @@ class Integration:
         return response.json()
 
     def get_test_statuses(self):
-        """Получение статусов для тест-кейсов"""
 
         url = f'{self.JIRA_URL}/rest/tests/1.0/project/{self.JIRA_PROJECT_ID}/testresultstatus'
         response = self._send_request_with_retries('GET', url)
@@ -213,7 +199,6 @@ class Integration:
         return response.json()
 
     def get_test_cycle_statuses(self):
-        """Получение статусов для тестового цикла"""
         url = f'{self.JIRA_URL}/rest/tests/1.0/project/{self.JIRA_PROJECT_ID}/testrunstatus'
         response = self._send_request_with_retries('GET', url)
 
@@ -225,7 +210,6 @@ class Integration:
 
 
     def set_test_case_statuses(self, statuses):
-        """Установка статусов для тест-кейсов"""
 
         url = f"{self.JIRA_URL}/rest/tests/1.0/testresult"
         response = self._send_request_with_retries('PUT', url, json=statuses)
@@ -236,7 +220,6 @@ class Integration:
         response.raise_for_status()
 
     def set_test_script_statuses(self, script_statuses):
-        """Установка статусов для шагов тест-кейсов"""
 
         url = f"{self.JIRA_URL}/rest/tests/1.0/testscriptresult"
         response = self._send_request_with_retries('PUT', url, json=script_statuses)
